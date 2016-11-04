1de9855710b9e4cd1b3a5fbc5e2a619bde78c552
Ruth MC Mason <ruthmcmason@hotmail.co.uk>
2016-11-04 15:07:23 +0000

    change body to button

--- assets/js/showhide.js
+++ assets/js/showhide.js
@@ -1,9 +1,9 @@
 module.exports = {
     init: function() {
-        $('body').on('click, change', '[data-show]', function(e) {
+        $('button').on('click, change', '[data-show]', function(e) {
             $($(this).data('show')).show();
         });
-        $('body').on('click, change', '[data-hide]', function(e) {
+        $('button').on('click, change', '[data-hide]', function(e) {
             $($(this).data('hide')).hide();
         });
     }
