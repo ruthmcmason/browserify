261c92fac10f4a89932ab1e0acdfa12d70a2c15e
Ruth MC Mason <ruthmcmason@hotmail.co.uk>
2016-11-04 16:37:04 +0000

    file path change

--- assets/js/main.js
+++
@@ -1,3 +0,0 @@
-var showHide = require('./assets/js/showhide.js');
-
-showHide();
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
+var browserify = require('browserify');
 
-gulp.task('default', function() {
-
-});
+gulp.task('build', ['browserify']);
+gulp.task('default', ['build']);
