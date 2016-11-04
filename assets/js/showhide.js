36b87d87528117a78c915cd7b9e5e1c8ce1639b0
Ruth MC Mason <ruthmcmason@hotmail.co.uk>
2016-11-04 15:10:34 +0000

    more sensible ids

--- assets/js/showhide.js
+++ assets/js/showhide.js
@@ -1,9 +1,9 @@
 module.exports = {
     init: function() {
-        $('button').on('click, change', '[data-show]', function(e) {
+        $('#show').on('click, change', '[data-show]', function(e) {
             $($(this).data('show')).show();
         });
-        $('button').on('click, change', '[data-hide]', function(e) {
+        $('#hide').on('click, change', '[data-hide]', function(e) {
             $($(this).data('hide')).hide();
         });
     }
--- dist/bundle.js
+++ dist/bundle.js
@@ -6,10 +6,10 @@ showHide();
 },{"./showhide.js":2}],2:[function(require,module,exports){
 module.exports = {
     init: function() {
-        $('body').on('click, change', '[data-show]', function(e) {
+        $('#show').on('click, change', '[data-show]', function(e) {
             $($(this).data('show')).show();
         });
-        $('body').on('click, change', '[data-hide]', function(e) {
+        $('#hide').on('click, change', '[data-hide]', function(e) {
             $($(this).data('hide')).hide();
         });
     }
